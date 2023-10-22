beb18844e514cd1f6f3dcc51bc48c7df6d329a4e
orondon <oscar.rondon.c@gmail.com>
2023-10-21 21:47:32 -0300

    Assesment-01 [ update linter to use ts-standard]

--- React/01-list-interaction/.eslintrc.cjs
+++ React/01-list-interaction/.eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'standard-with-typescript'
     
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -23,7 +24,13 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    'no-multiple-empty-lines': 'off',
+    'padded-blocks': 'off'
   },
   settings: { react: { version: 'detect' } },
 }
--- React/01-list-interaction/src/App.tsx
+++ React/01-list-interaction/src/App.tsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 
 import './App.css'
 
-interface ListItem{
-  id: string,
-  timestamp: number,
-  text: string,
+interface ListItem {
+  id: string
+  timestamp: number
+  text: string
 
 }
 
 const INITIAL_ITEMS: ListItem[] = [
   {
     id: crypto.randomUUID(),
-    timestamp:Date.now(),
+    timestamp: Date.now(),
     text: 'Item 1'
   },
   {
@@ -22,30 +22,29 @@ const INITIAL_ITEMS: ListItem[] = [
   },
   {
     id: crypto.randomUUID(),
-    timestamp:Date.now(),
+    timestamp: Date.now(),
     text: 'Item 3'
   }
 ]
 
-function App() {
-  
+function App () {
   const [items, setItems] = useState(INITIAL_ITEMS)
-  
+
   const onHandleSubmit = (evnt: React.FormEvent<HTMLFormElement>) => {
     evnt.preventDefault()
-    const { elements }:HTMLFormElement = evnt.currentTarget
+    const { elements }: HTMLFormElement = evnt.currentTarget
     const input = elements.namedItem('inputText')
     const isInput = input instanceof HTMLInputElement
     if (isInput) {
       console.log(input.value)
-      const newItem = {id: crypto.randomUUID(), timestamp: Date.now(), text: input.value}
+      const newItem = { id: crypto.randomUUID(), timestamp: Date.now(), text: input.value }
       setItems(prev => [...prev, newItem])
       input.value = ''
       input.focus()
     }
   }
-  
-  const onHandleRemoveItem = (id: string) => () =>{
+
+  const onHandleRemoveItem = (id: string) => () => {
     setItems(prev => prev.filter(item => item.id !== id))
   }
 
@@ -66,21 +65,22 @@ function App() {
         <section>
           <h2>List</h2>
             {
-              items.length === 0 ? (<strong>No items in the list...</strong>) :
-              (
+              items.length === 0
+                ? (<strong>No items in the list...</strong>)
+                : (
                 <ul>
                   {
                     items.map(item => {
                       return (
                         <li key={item.id}>
                           {item.text}
-                          <button  onClick={onHandleRemoveItem(item.id)}>remove</button>
+                          <button onClick={onHandleRemoveItem(item.id)}>remove</button>
                         </li>
                       )
                     })
                   }
                 </ul>
-              )
+                  )
             }
         </section>
       </div>
